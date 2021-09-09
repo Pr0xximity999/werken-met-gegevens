d98bbefa467e047336fb93ecec2676ab921af721
Thomas Koman <99066789@mydavinci.nl>
2021-09-09 11:25:50 +0200

    ik will pizza

--- calculations/feestlunch.py
+++ calculations/feestlunch.py
@@ -1,9 +1,10 @@
-krosantje = 0.39
-stokbrood = 2.78
-kortingsbon = 0.50
-krosantjeAantal = 17
-stokbroodaAntal = 2
-kortingsbonAantal = 3
+krosantjeAantal = float(input("Hoeveel kwaksons wilt gei kopen? >>"))
+stokbroodAantal = float(input("Hoeveel broodstokken wil je aanschaffen? >>"))
+kortingsbonAantal = float(input("Hoeveel bonnen met een neppe waarde heeft u in bezit? >>"))
+krosantje = float(input("Hoeveel kost elk kwaksonnetje? >>"))
+stokbrood = float(input("Hoeveel kost elk stokje brood? >>"))
+kortingsbon = float(input("Hoeveel is de neppe kortingswaarde van die stukjes papier waard? >>"))
+
 kosten = 17 * krosantje + 2 * stokbrood - 3 * kortingsbon
 print("de kosten zijn " + str(kosten) + " euro, als je ")
-print(str(krosantjeAantal) + " krosantjes en "+ str(stokbroodaAntal)+" stokbroden koopt met "+ str(kortingsbonAantal)+" kortingsbonnen!")
+print(str(krosantjeAantal) + " krosantjes en "+ str(stokbroodAantal)+" stokbroden koopt met "+ str(kortingsbonAantal)+" kortingsbonnen!")
--- calculations/speelhal-dag.py
+++ calculations/speelhal-dag.py
@@ -1,7 +1,7 @@
-personen = 4
+personen = float(input("Hoeveel vrienden heb je(plus jezelf):"))
 speelHalKosten = 7.45
 vrSeatKosten = 0.37
-tijdVr = 45
+tijdVr = float(input("Voor hoeveel minuten wil je deze realiteit ontsnappen: "))
 
 kosten = personen * (speelHalKosten + (tijdVr / 5) * vrSeatKosten)
 print("De dag kost in totaal een geweldige, supere, toffe, leuke, hilarische " + str(kosten) + " euro als je met " + str(personen) + " bent en ze allemaal " + str(tijdVr) + " minuten in de vr booth zitten!")
