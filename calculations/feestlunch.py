a8f4f0e6ea79ab15d9ea99da4233d484e5a33efa
Thomas Koman <99066789@mydavinci.nl>
2021-09-09 10:16:20 +0200

    KAAS

--- calculations/feestlunch.py
+++ calculations/feestlunch.py
@@ -1,5 +1,9 @@
 krosantje = 0.39
 stokbrood = 2.78
 kortingsbon = 0.50
+krosantjeAantal = 17
+stokbroodaAntal = 2
+kortingsbonAantal = 3
 kosten = 17 * krosantje + 2 * stokbrood - 3 * kortingsbon
-print("de kosten zijn " + str(kosten) + " euro")
+print("de kosten zijn " + str(kosten) + " euro, als je ")
+print(str(krosantjeAantal) + " krosantjes en "+ str(stokbroodaAntal)+" stokbroden koopt met "+ str(kortingsbonAantal)+" kortingsbonnen!")
--- calculations/speelhal-dag.py
+++ calculations/speelhal-dag.py
@@ -1,8 +1,8 @@
 personen = 4
 speelHalKosten = 7.45
 vrSeatKosten = 0.37
-tijdVr = 45 / 5
+tijdVr = 45
 
-kosten = personen * (speelHalKosten + (tijdVr) * vrSeatKosten)
-print("De dag kost in totaal " + str(kosten) + " euro")
+kosten = personen * (speelHalKosten + (tijdVr / 5) * vrSeatKosten)
+print("De dag kost in totaal een geweldige, supere, toffe, leuke, hilarische " + str(kosten) + " euro als je met " + str(personen) + " bent en ze allemaal " + str(tijdVr) + " minuten in de vr booth zitten!")
 #negeer de .00000000000000000000005
